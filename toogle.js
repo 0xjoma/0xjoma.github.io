94dedd371bb7f7f48fdb102526dc810cff574e19
0xjoma <99035372+0xjoma@users.noreply.github.com>
2023-09-11 21:42:20 -0500

    changed to jekyll

--- toogle.js
+++
@@ -1,8 +0,0 @@
-function toggleName() {
-    var nameElement = document.getElementById("nameCaption")
-    if (nameElement.innerHTML === "0xjoma") {
-        nameElement.innerHTML = "001100000111100001101010011011110110110101100001";
-    } else {
-        nameElement.innerHTML = "0xjoma";
-    }
-}
